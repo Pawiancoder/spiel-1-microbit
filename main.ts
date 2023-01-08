de243d22e471902cff9c79cfbec21a8459c32971
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2023-01-08 21:08:04 +0100

    Kommentare hinzugefüge

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 
-let spriteX = 2; //links / rechts
+let spriteX = 2; //links / rechts 
 let spriteY = 3; //oben / unten
 
 input.onButtonPressed(Button.A, () => {
-    led.unplot(spriteX, spriteY);
-    spriteX += 1;
-    led.plot(spriteX, spriteY);
+    led.unplot(spriteX, spriteY); //alte Koordinaten
+    spriteX += 1; //X - Koordinate + 1
+    led.plot(spriteX, spriteY); //neue Koordinaten
 });
 
-led.plot(spriteX, spriteY);
+led.plot(spriteX, spriteY); // Sprite wird geladen
